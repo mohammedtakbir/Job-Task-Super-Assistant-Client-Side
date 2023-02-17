fb9af1760ca054edebc2f49c7ada71c6ba8ebf95
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-17 15:24:10 +0600

    cancel this project

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { FaAngleDown } from "react-icons/fa";
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+    const ref = useRef(null);
+    const [open, setOpen] = useState(false);
+
+    const handleOpenBar = (value) => {
+        setOpen(value);
+    }
+
+    useEffect(() => {
+        document.addEventListener('click', handleClickOutside, true)
+    }, [])
+
+    const handleClickOutside = (e) => {
+        console.log(ref.current.contains(e.target))
+        if (!ref.current.contains(e.target)) {
+            setOpen(false);
+        }
+    }
+
     return (
-        <section className='h-[75px] bg-[#FAFAFA] leading-[75px]'>
-            <div className='max-w-[1200px] mx-auto flex justify-between'>
+        <section className='py-2 bg-[#FAFAFA]'>
+            <div className='max-w-[1300px] mx-auto flex justify-between items-center relative'>
                 <div className='flex items-center'>
-                    <img className='h-[50px]' src="https://png.pngtree.com/png-vector/20220709/ourmid/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.png" alt="" />
-                    <ul className='flex ml-10'>
-                        <li>Products</li>
-                        <li>Pricing</li>
-                        <li>Guides</li>
-                        <li>Templates</li>
+                    <div className='flex items-center'>
+                        <img className='h-[50px] w-[50px]' src="https://internal-images.s3.ap-south-1.amazonaws.com/BoloForms+Logo+(1)/2.svg" alt="" />
+                        <p className='font-semibold text-[14px]'>BoloForms</p>
+                    </div>
+                    <ul className='flex ml-12'>
+                        <li ref={ref} onClick={() => handleOpenBar(!open)} className='text-[14px] font-semibold mr-[40px] cursor-pointer'>Products <FaAngleDown className='inline' />
+                            <div onClick={(e) => e.stopPropagation()} className={`absolute border p-5 top-[50px] cursor-auto ${open ? 'block' : 'hidden'}`}>
+                                <div className='flex'>
+                                    <h2 className='mr-3'>Icon</h2>
+                                    <div>
+                                        <h3>Form Approval</h3>
+                                        <p>Create interactive workflows, approval flows & automate processes using Google Forms.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className='text-[14px] font-semibold mr-[40px] cursor-pointer'>Pricing</li>
+                        <li className='text-[14px] font-semibold mr-[40px] cursor-pointer'>Guides</li>
+                        <li className='text-[14px] font-semibold mr-[40px] cursor-pointer'>Templates</li>
                     </ul>
                 </div>
                 <div>
-                    <button>Install Now</button>
-                    <button>BoloForms Premium</button>
+                    <button className='mr-5 border'>Install Now</button>
+                    <button className=' border'>BoloForms Premium</button>
                 </div>
             </div>
         </section>
